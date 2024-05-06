8814162df595ad7d661a9c9a4a31b4f732623371
LiquidZulu <liquidzulu@pm.me>
2024-05-06 22:41:58 +0100

    fix tweening for glow

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,14 +1,26 @@
 import { makeScene2D, Txt } from "@motion-canvas/2d";
+import { Color, tween, createRef, easeInOutCubic } from "@motion-canvas/core";
+import { easeInOutCubic } from "@motion-canvas/core";
 import { colors, effects, glow } from "../";
 
 export default makeScene2D(function* (view) {
   view.fill(colors.bg);
 
+  const txt = createRef<Txt>();
+
   view.add(
     effects(
-      <Txt fontFamily="Oswald" fontSize={150} fill="red">
+      <Txt ref={txt} fontFamily="Oswald" fontSize={150} fill={new Color("red")}>
         BLAH
       </Txt>
     ).glow()
   );
+
+  yield* tween(2, (value) => {
+    txt().fill(
+      Color.lerp(new Color("red"), new Color("blue"), easeInOutCubic(value))
+    );
+  });
+
+  yield* txt().fontSize(120, 3);
 });
