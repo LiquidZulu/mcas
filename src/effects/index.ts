adb00b0128c1cf8893852bb403b6647ac0975d05
LiquidZulu <liquidzulu@pm.me>
2024-05-06 22:34:36 +0100

    effects function

--- src/effects/index.ts
+++ src/effects/index.ts
@@ -1,1 +1,10 @@
+import { Node } from "@motion-canvas/2d";
+import { glow } from "./glow";
+
 export * from "./glow";
+
+export function effects(n: Node) {
+  return {
+    glow: () => glow(n),
+  };
+}
--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,14 +1,14 @@
 import { makeScene2D, Txt } from "@motion-canvas/2d";
-import { colors, glow } from "../";
+import { colors, effects, glow } from "../";
 
 export default makeScene2D(function* (view) {
   view.fill(colors.bg);
 
   view.add(
-    glow(
+    effects(
       <Txt fontFamily="Oswald" fontSize={150} fill="red">
         BLAH
       </Txt>
-    )
+    ).glow()
   );
 });
