f212f9708c7e850c1dba0290c978c7a7f586e4f1
LiquidZulu <liquidzulu@pm.me>
2024-10-23 17:10:46 +0100

    update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        ignores: ['lib', 'node_modules', 'dist', 'cli/assets/*'],
+        env: {
+            browser: true,
+            es2021: true,
+        },
+        ignorePatterns: [
+            '**/*.js',
+            '**/*.d.ts',
+            'packages/template',
+            'packages/create/template-*',
+        ],
+        extends: [
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+        ],
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
+        rules: {
+            'require-yield': 'off',
+            '@typescript-eslint/explicit-member-accessibility': 'error',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-namespace': 'off',
+            'tsdoc/syntax': 'warn',
+        },
+    },
+];
