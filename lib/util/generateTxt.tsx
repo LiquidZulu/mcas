dc62774e1edd12b927c7f1eb7be178dd93c5da60
LiquidZulu <liquidzulu@pm.me>
2024-12-06 14:44:18 +0000

    Add optional props

--- lib/util/generateTxt.tsx
+++ lib/util/generateTxt.tsx
@@ -1,11 +1,15 @@
-import { TRichText } from '../';
+import { McasTxtProps, TRichText } from '../';
 import { McasTxt as Txt } from '../';
 
-export const generateTxt = (text: TRichText[]): Txt =>
+export const generateTxt = (text: TRichText[], props?: McasTxtProps): Txt =>
     (
         <>
             {text.map(t =>
-                typeof t == 'string' ? <Txt text={t} /> : <Txt {...t} />,
+                typeof t == 'string' ? (
+                    <Txt text={t} {...props} />
+                ) : (
+                    <Txt {...t} {...props} />
+                ),
             )}
         </>
     ) as Txt;
