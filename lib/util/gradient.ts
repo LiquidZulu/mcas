e046e2227942585366ea4bbf4fc93d42e39b6c41
LiquidZulu <liquidzulu@pm.me>
2025-01-23 08:43:30 +0000

    local gradients

--- lib/util/gradient.ts
+++ lib/util/gradient.ts
@@ -1,10 +1,15 @@
 import { Gradient } from '@motion-canvas/2d';
 import {
     Color,
+    createSignal,
     PossibleColor,
     PossibleVector2,
+    Reference,
+    SimpleSignal,
     Vector2,
+    Vector2Signal,
 } from '@motion-canvas/core';
+import { mkSignal } from './misc';
 
 type GradientDirection =
     | 'left'
@@ -15,6 +20,14 @@ type GradientDirection =
     | 'top-right'
     | 'bottom-left'
     | 'bottom-right'
+    | 'l'
+    | 'r'
+    | 'u'
+    | 'd'
+    | 'tl'
+    | 'tr'
+    | 'bl'
+    | 'br'
     | 'n'
     | 'north'
     | 'nw'
@@ -59,15 +72,37 @@ export function mkGradient(
     colorOrDirectionOrTo: PossibleColor | GradientDirection | PossibleVector2,
     ...colors: [PossibleColor, ...PossibleColor[]]
 ): Gradient {
+    return mkGradientExplicit(
+        ...getGradientExplicit(
+            directionOrWidthOrFrom,
+            colorOrDirectionOrTo,
+            ...colors,
+        ),
+    );
+}
+
+function getGradientExplicit(
+    directionOrWidthOrFrom: GradientDirection | number | PossibleVector2,
+    colorOrDirectionOrTo: PossibleColor | GradientDirection | PossibleVector2,
+    ...colors: [PossibleColor, ...PossibleColor[]]
+): [
+    SimpleSignal<Vector2>,
+    SimpleSignal<Vector2>,
+    ...[PossibleColor, ...PossibleColor[]],
+] {
     // definitely a GradientDirection
     if (typeof directionOrWidthOrFrom === 'string') {
-        return mkGradientExplicit(
+        return [
             ...getGradientPoints(
                 directionOrWidthOrFrom as unknown as GradientDirection,
                 2203,
             ),
             ...[colorOrDirectionOrTo as unknown as PossibleColor, ...colors],
-        );
+        ] as unknown as [
+            SimpleSignal<Vector2>,
+            SimpleSignal<Vector2>,
+            ...[PossibleColor, ...PossibleColor[]],
+        ];
     }
 
     // either direction or width or number color
@@ -77,38 +112,42 @@ export function mkGradient(
         // it is either a number direction or a width,
         // assume its a width if it is greater than 1
         if (directionOrWidthOrFrom > 1) {
-            return mkGradientExplicit(
+            return [
                 ...getGradientPoints(
                     colorOrDirectionOrTo as unknown as GradientDirection,
                     directionOrWidthOrFrom as unknown as number,
                 ),
                 ...colors,
-            );
+            ];
         }
 
         // its a number direction
-        return mkGradientExplicit(
+        return [
             ...getGradientPoints(
                 directionOrWidthOrFrom as unknown as GradientDirection,
                 2203,
             ),
             ...[colorOrDirectionOrTo as unknown as PossibleColor, ...colors],
-        );
+        ] as unknown as [
+            SimpleSignal<Vector2>,
+            SimpleSignal<Vector2>,
+            ...[PossibleColor, ...PossibleColor[]],
+        ];
     }
 
     // it is not mkGradient(direction, ...colors)
     // or mkGradient(width, direction, ...colors)
 
-    return mkGradientExplicit(
-        directionOrWidthOrFrom,
-        colorOrDirectionOrTo as unknown as PossibleVector2,
+    return [
+        mkSignal(directionOrWidthOrFrom) as SimpleSignal<Vector2>,
+        mkSignal(colorOrDirectionOrTo) as unknown as SimpleSignal<Vector2>,
         ...colors,
-    );
+    ];
 }
 
 function mkGradientExplicit(
-    from: PossibleVector2,
-    to: PossibleVector2,
+    from: SimpleSignal<Vector2>,
+    to: SimpleSignal<Vector2>,
     ...colors: [PossibleColor, ...PossibleColor[]]
 ): Gradient {
     let stops = [];
@@ -150,19 +189,23 @@ function mkGradientExplicit(
 function getGradientPoints(
     direction: GradientDirection,
     width: number,
-): [Vector2, Vector2] {
+): [SimpleSignal<Vector2>, SimpleSignal<Vector2>] {
     // for whatever reason, it ends up with 0 as to the left, so I need to correct it by a quarter turn
     const nd = getNormalisedDirection(direction) + 0.25;
     const r = width / 2;
     return [
-        new Vector2([
-            r * Math.cos(nd * (2 * Math.PI)),
-            r * Math.sin(nd * (2 * Math.PI)),
-        ]),
-        new Vector2([
-            r * Math.cos((nd + 0.5) * (2 * Math.PI)),
-            r * Math.sin((nd + 0.5) * (2 * Math.PI)),
-        ]),
+        createSignal(
+            new Vector2([
+                r * Math.cos(nd * (2 * Math.PI)),
+                r * Math.sin(nd * (2 * Math.PI)),
+            ]),
+        ),
+        createSignal(
+            new Vector2([
+                r * Math.cos((nd + 0.5) * (2 * Math.PI)),
+                r * Math.sin((nd + 0.5) * (2 * Math.PI)),
+            ]),
+        ),
     ];
 }
 
@@ -170,34 +213,42 @@ function getGradientPoints(
 function getNormalisedDirection(direction: GradientDirection): number {
     if (typeof direction === 'string') {
         switch (direction) {
+            case 'u':
             case 'up':
             case 'n':
             case 'north':
                 return 0;
+            case 'tr':
             case 'top-right':
             case 'nw':
             case 'northwest':
                 return 1 / 8;
+            case 'r':
             case 'right':
             case 'w':
             case 'west':
                 return 2 / 8;
+            case 'br':
             case 'bottom-right':
             case 'sw':
             case 'southwest':
                 return 3 / 8;
+            case 'd':
             case 'down':
             case 's':
             case 'south':
                 return 4 / 8;
+            case 'bl':
             case 'bottom-left':
             case 'se':
             case 'southeast':
                 return 5 / 8;
+            case 'l':
             case 'left':
             case 'e':
             case 'east':
                 return 6 / 8;
+            case 'tl':
             case 'top-left':
             case 'ne':
             case 'northeast':
@@ -206,4 +257,62 @@ function getNormalisedDirection(direction: GradientDirection): number {
     }
 
     return direction % 1;
+}
+
+export function localGradient(ref: Reference<any>) {
+    // mkGradient([0,0], [500,100], rose500, ...)
+    function mkLocalGradient(
+        from: PossibleVector2,
+        to: PossibleVector2,
+        ...colors: [PossibleColor, ...PossibleColor[]]
+    ): Gradient;
+
+    // mkGradient(500, "top-left", rose500, emerald300, sky700, ...)
+    function mkLocalGradient(
+        gradientWidth: number,
+        direction: GradientDirection,
+        ...colors: [PossibleColor, ...PossibleColor[]]
+    ): Gradient;
+
+    // assume full diagonal width
+    // mkGradient('top-left', rose500, ...)
+    function mkLocalGradient(
+        direction: GradientDirection,
+        ...colors: [PossibleColor, ...PossibleColor[]]
+    ): Gradient;
+
+    function mkLocalGradient(
+        directionOrWidthOrFrom: GradientDirection | number | PossibleVector2,
+        colorOrDirectionOrTo:
+            | PossibleColor
+            | GradientDirection
+            | PossibleVector2,
+        ...colors: [PossibleColor, ...PossibleColor[]]
+    ) {
+        const [from, to, ...cols] = getGradientExplicit(
+            directionOrWidthOrFrom,
+            colorOrDirectionOrTo,
+            ...colors,
+        );
+
+        const f = createSignal(() => new Vector2(from()));
+        const t = createSignal(() => new Vector2(to()));
+
+        const squash = [
+            createSignal(() => ref().width() / ref().view().width()),
+            createSignal(() => ref().height() / ref().view().height()),
+        ];
+
+        return mkGradientExplicit(
+            createSignal(
+                () => new Vector2([f().x * squash[0](), f().y * squash[1]()]),
+            ),
+            createSignal(
+                () => new Vector2([t().x * squash[0](), t().y * squash[1]()]),
+            ),
+            ...cols,
+        );
+    }
+
+    return mkLocalGradient;
 }
